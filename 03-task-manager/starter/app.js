36a7053f5e87192b1862d37dc626e4fcf3f5e882
Alina Mats <npnote8@gmail.com>
2023-07-11 22:11:42 -0700

    answers for lesson 6

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,18 +3,18 @@ const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // middleware
-
+app.use(express.static("./public"));
 app.use(express.json());
 
 // routes
 
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
-
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 // app.get('/api/v1/tasks')        - get all the tasks
 // app.post('/api/v1/tasks')       - create a new task
@@ -22,7 +22,9 @@ app.use("/api/v1/tasks", tasks);
 // app.patch('/api/v1/tasks/:id')  - update task
 // app.delete('/api/v1/tasks/:id')  - delete task
 
-const port = 3000;
+// handles 404 and sends custom response
+
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
